78d38888c363db0a6079c1de1cdfa907e905073e
ashfaq-mulla <ashfaqmulla963@gmail.com>
2025-02-08 20:59:30 +0530

    Basic react using Vite

---
+++ 01ReactWithVite/src/App.jsx
@@ -0,0 +1,19 @@
+import FirstFile from "./FirstFile"
+
+function App() {
+ // initial code 
+  // return (
+  //   <h1>First Project with Vite by Ashfaq</h1>
+  // )
+
+  // new code after FirstFile and firstFunction
+  return (
+    
+    <>
+    <FirstFile />
+    <h1>second element in separate jsx fragment</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01ReactWithVite/src/FirstFile.jsx
@@ -0,0 +1,12 @@
+// function firstFunction(){
+    function FirstFile(){
+    return (
+        // <> </>This is a JSX fragment tag. While workingv with vite, wrap the elements in JSX fragment 
+        <>  
+        <h2>A new first function to understand how the React App can be rendered on browser.</h2>
+        <p>This function or this file is actually a new React component.</p>
+        </> //End of JSX fragment
+    )
+}
+
+export default FirstFile
